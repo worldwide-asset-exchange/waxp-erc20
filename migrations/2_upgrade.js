9803e3eb8b07bb841f4ad14ce2b2bd7e4af8f70e
Aaron Caswell <aaron@captureplay.com>
2022-10-11 16:17:50 -0700

    og wax swap deployment

--- migrations/2_upgrade.js
+++ migrations/2_upgrade.js
@@ -1,9 +1,9 @@
 const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
-const contractInfo = require('../.openzeppelin/rinkeby.json');
 
 const WAXPERC20UpgradeSafe = artifacts.require('WAXPERC20UpgradeSafe');
 
 module.exports = async function (deployer) {
+  const contractInfo = require(`../.openzeppelin/${deployer.network}.json`);
   if (deployer.network == "test" || deployer.network == "development" || deployer.network == "soliditycoverage" ) return;
   const existing = await WAXPERC20UpgradeSafe.at(contractInfo.proxies[0].address);
   const instance = await upgradeProxy(existing.address, WAXPERC20UpgradeSafe, { deployer });
